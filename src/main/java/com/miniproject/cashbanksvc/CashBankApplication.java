6c5938b7512bd02a59e21308e11d75ebe8d09420
Rafi Mufadhal Difany <55683417+rafimdifany@users.noreply.github.com>
2023-04-05 14:36:20 +0700

    Initial Commit

---
+++ src/main/java/com/miniproject/cashbanksvc/CashBankApplication.java
@@ -0,0 +1,13 @@
+package com.miniproject.cashbanksvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashBankApplication.class, args);
+	}
+
+}
